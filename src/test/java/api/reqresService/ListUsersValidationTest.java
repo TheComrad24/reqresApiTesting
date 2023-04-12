740eb5747133004277d83196f9133694e7b4911f
chertsov <chertsov0224@yandex.ru>
2023-04-12 16:15:42 +0600

    Добавлены Json схемы и тесты валидации для ListUsers и UpdateUser

---
+++ src/test/java/api/reqresService/ListUsersValidationTest.java
@@ -0,0 +1,23 @@
+package api.reqresService;
+
+import api.reqresService.config.EndPoints;
+import api.reqresService.config.ReqresServiceSpecifications;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class ListUsersValidationTest {
+
+    @Test
+    public void successListUsersValidTest(){
+        ReqresServiceSpecifications.installSpecification(ReqresServiceSpecifications.reqSpec(EndPoints.baseUrl),
+                ReqresServiceSpecifications.respSpec200());
+
+        given()
+                .when()
+                .get(EndPoints.usersEndpoint, 2)
+                .then().log().all()
+                .assertThat().body(matchesJsonSchemaInClasspath("reqresService/ListUsersShema.json"));
+    }
+}
---
+++ src/test/java/api/reqresService/UpdateUserValidationTest.java
@@ -0,0 +1,27 @@
+package api.reqresService;
+
+import api.reqresService.config.EndPoints;
+import api.reqresService.config.ReqresServiceSpecifications;
+import api.reqresService.users.request.UpdateUserReq;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+
+public class UpdateUserValidationTest {
+
+    @Test
+    public void updateUserValidationTest(){
+        ReqresServiceSpecifications.installSpecification(ReqresServiceSpecifications.reqSpec(EndPoints.baseUrl),
+                ReqresServiceSpecifications.respSpec200());
+
+        UpdateUserReq updateUserReq = new UpdateUserReq("morpheus","zion resident");
+        given()
+                .body(updateUserReq)
+                .when()
+                .put(EndPoints.usersUpdateEndpoint,2)
+                .then().log().all()
+                .assertThat().body(matchesJsonSchemaInClasspath("reqresService/UpdateUserShema.json"));
+    }
+}
